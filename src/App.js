a7020546fa983059826a394f533536a3ce240bc8
John Muir <johnmuir@JohnsWorkLaptop.lan>
2022-05-12 13:44:09 +0100

    add advanced calculator

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import { evaluate } from "mathjs";
 import './App.css';
 
 function App() {
-  const [sum, setSum] = useState("")
-  const [buttons, setButtons] = useState(["7","8","9","C","4","5","6","*","1","2","3","/","+","0","-","="])
+  const [sum, setSum] = useState("");
+  const [advanced, setAdvanced] = useState(false);
+  const buttons = ["7","8","9","C","4","5","6","*","1","2","3","/","+","0","-","="];
+  const advancedButtons = ["7","8","9","C","(","4","5","6","x",")","1","2","3","÷","√","+","0","-",".","="]
 
   const handleClick = (string) => {
       if(string === "="){
-          setSum(evaluate(sum))
+        let expression = sum;
+        let changeSqrt = expression.replace("√", "sqrt");
+        let changeDivide = changeSqrt.replace("÷", "/");
+        let changeMultiply = changeDivide.replace("x", "*");
+        setSum(evaluate(changeMultiply))
       } else if(string === "C"){
           setSum("")
+      } else if(string === "√"){
+        setSum(sum + string + "(")
       } else {
           setSum(sum + string)
       }
@@ -18,13 +26,22 @@ function App() {
 
   return (
     <div className="app">
+      <button onClick={() => setAdvanced(!advanced)}>Advanced Calculator</button>
       <h1>{sum}</h1>
-      <div className="buttonWrap">
-        {buttons.map((button, index) => {
-          return (
-            <button onClick={() => handleClick(button)} key={index}>{button}</button>
-          )
-        })}
+      <div className={advanced ? "advancedButtonWrap" : "buttonWrap"}>
+        {advanced ? 
+          advancedButtons.map((button, index) => {
+            return (
+              <button onClick={() => handleClick(button)} key={index}>{button}</button>
+            )
+          }) :
+
+          buttons.map((button, index) => {
+            return (
+              <button onClick={() => handleClick(button)} key={index}>{button}</button>
+            )
+          })
+        }
       </div>
     </div>
   );
